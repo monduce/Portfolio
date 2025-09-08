2f12fb7ecb188e030532c108bc13be2d32977801
monduce <ttc2350sg0026@edu.tech.ac.jp>
2025-09-08 16:38:23 +0900

    RunGame_Portfolio

---
+++ u20250906_Portfolio_Test/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-DropDownBlock-0.1";
+const contentToCache = [
+    "Build/u20250906_Portfolio_Test.loader.js",
+    "Build/u20250906_Portfolio_Test.framework.js.unityweb",
+    "Build/u20250906_Portfolio_Test.data.unityweb",
+    "Build/u20250906_Portfolio_Test.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
